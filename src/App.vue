8db42290c6417fd7759f414657f160f71a4eaae4
mojomomo <2428015329@qq.com>
2023-08-03 13:37:21 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import RegistrationForm from "./components/RegistrationForm.vue"
+</script>
+
+<template>
+  <div class="container">
+    <h2>
+      <span>网信委招新报名系统</span>
+      <img src="./assets/logo.png" alt="">
+    </h2>
+
+<RegistrationForm></RegistrationForm>
+
+    
+  </div>
+
+</template>
+
+<style scoped>
+.container {
+  padding: 10px 20px;
+}
+h2 {
+    display: flex;
+    align-items: center;
+    
+}
+img {
+  width: 40px;
+  height: 40px;
+  display: inline;
+}
+</style>
