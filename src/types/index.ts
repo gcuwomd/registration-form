82a0738dc425d5864a6c4d16a57c30192598f001
Xu <2077203929@qq.com>
2023-08-04 15:40:42 +0800

    1

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
- interface IApply {
+ export interface IApply {
   id: number | null;
   name: string | null;
   college: string | null;
@@ -8,64 +8,8 @@
   introduction: string | null;
   picture?: string | null;
 }
- interface IOption {
+ export interface IOption {
   value: number;
   label: string;
   disabled?:boolean;
 }
-const collegeOptions: Array<IOption> = [
-  {
-      label: "计算机工程学院",
-      value: 1,
-  },
-  {
-      label: "电气工程学院",
-      value: 2,
-  },
-  {
-      label: "外国语学院",
-      value: 3,
-  },
-  {
-      label: "大数据学院",
-      value: 4,
-  },
-  {
-      label: "管理学院",
-      value: 5,
-  },
-  {
-      label: "通信工程学院",
-      value: 6,
-  },
-  {
-      label: "国际商学院",
-      value: 7,
-  },
-  {
-      label: "电子信息工程学院",
-      value: 8,
-  },
-  {
-      label: "土木工程学院",
-      value: 9,
-  },
-];
-const sectionOptions: Array<IOption> = [
-  {
-      label: "网站运维部",
-      value: 11,
-  },
-  {
-      label: "行政秘书部",
-      value: 12,
-  },
-  {
-      label: "网络运维部",
-      value: 13,
-  },
-  {
-      label: "信息化运维部",
-      value: 14,
-  },
-];
